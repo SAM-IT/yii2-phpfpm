9a387d557845dca1418e18833cc4b4a993dadb03
Sam Mousa <sam@mousa.nl>
2018-08-08 09:47:32 +0200

    Abort container launch if initialization command fails

--- src/Module.php
+++ src/Module.php
@@ -222,7 +222,7 @@ SH;
         }
 
         foreach($this->initializationCommands as $route) {
-            $result[] = "$script $route --interactive=0";
+            $result[] = "$script $route --interactive=0 || exit";
         }
         $result[] = 'exec php-fpm7 --force-stderr --fpm-config /php-fpm.conf';
         return \implode("\n", $result);
